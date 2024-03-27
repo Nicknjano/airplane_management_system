de5d5cddb421bafe6fc0d2047301eb9f3b2c167d
Nicknjano <122860272+Nicknjano@users.noreply.github.com>
2024-03-27 23:09:56 +0300

    Update wsgi.py for deployment

--- AIRLINE_MANAGEMENT_SYSTEM/wsgi.py
+++ AIRLINE_MANAGEMENT_SYSTEM/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AIRLINE_MANAGEMENT_SYSTEM.settings")
 
 application = get_wsgi_application()
+
+app = application
